a6f3ec16b22fb1e663cd08ff1195178b1a3b3f1a
David S <hin7788966558@gmail.com>
2021-06-13 08:32:18 -0700

    added GET num_phone and Num_Tablet in RepairStat.py

--- Lab5/repair_stats.py
+++ Lab5/repair_stats.py
@@ -1,7 +1,7 @@
 class RepairStats:
     """ Statistics on a Mobile Device Repair Shop Inventory """
 
-    def __init__(self, num_pending_repairs, num_completed_repairs, total_profit):
+    def __init__(self, num_pending_repairs, num_completed_repairs, total_profit, num_phones, num_tablets):
         """ Initialize the data values """
 
         if num_pending_repairs is None or type(num_pending_repairs) != int:
@@ -16,15 +16,31 @@ class RepairStats:
             raise ValueError("Invalid repairs profit")
         self._total_profit = total_profit
 
+        if num_phones is None or type(num_phones) != int:
+            raise ValueError("Invalid Phone number value")
+        self._num_phones = num_phones
+
+        if num_tablets is None or type (num_tablets) != int:
+            raise ValueError("Invalid Tablet number value")
+        self._num_tablets = num_tablets
+
 
     def get_num_pending_repairs(self):
         """ Returns the number of pending repairs """
         return self._num_pending_repairs
 
     def get_num_completed_repairs(self):
-        """ Returns the number cof completed repairs """
+        """ Returns the number of completed repairs """
         return self._num_completed_repairs
 
     def get_total_profit(self):
         """ Returns the total profit for all the completed repairs """
         return self._total_profit
+
+    def get_num_phones(self):
+        """Returns the number of phones"""
+        return self._num_phones
+
+    def get_num_tablets(self):
+        """Return the number of Tablets"""
+        return self._num_tablets
